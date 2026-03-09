2baf4f7057d29a58853072b503474cdb6b66b65c
jaimegarcaa <jaime.garciagonzalez@estudiante.uam.es>
2026-03-09 18:13:39 +0100

    enlace que pasa ejercicio4

--- Enlace.java
+++ Enlace.java
@@ -1,12 +1,12 @@
-package red_social;
+package redSocial;
 
 public class Enlace {
-	private String usuarioOrigen;
-	private String usuarioDestino;
+	private Usuario usuarioOrigen;
+	private Usuario usuarioDestino;
 	private int coste;
 	static int sumaDeCostes=0;
 	
-	public Enlace(String uOrigen, String uDestino, int coste) {
+	public Enlace(Usuario uOrigen, Usuario uDestino, int coste) {
 		this.usuarioOrigen = uOrigen;
 		this.usuarioDestino = uDestino;
 		if(coste<=0) {
@@ -16,32 +16,32 @@ public class Enlace {
 			this.coste= coste;
 		}
 		//se suma el coste de la nueva instancia al atributo estatico sumaDeCostes
-		this.sumaDeCostes+=this.coste;
+		Enlace.sumaDeCostes+=this.coste;
 	}
-	public Enlace(String uOrigen, String uDestino) {
+	public Enlace(Usuario uOrigen, Usuario uDestino) {
 		this.usuarioOrigen = uOrigen;
 		this.usuarioDestino = uDestino;
 		this.coste=1;
 		//se suma el coste de la nueva instancia al atributo estatico sumaDeCostes
-		this.sumaDeCostes+=this.coste;
+		Enlace.sumaDeCostes+=this.coste;
 	}
-	public String getUsuarioOrigen(){
+	public Usuario getUsuarioOrigen(){
 		return this.usuarioOrigen;
 	}
-	public String getUsuarioDestino() {
+	public Usuario getUsuarioDestino() {
 		return this.usuarioDestino;
 	}
 	public int getCoste() {
 		return this.coste;
 	}
 	public void cambiarDestino(Usuario nuevoUsuario, int nuevoCoste ) {
-		this.usuarioDestino= Usuario.getUsuario();
+		this.usuarioDestino= nuevoUsuario;
 		//para actualizar la suma total de costes en caso de cambiar el coste de un enlace, restamos el coste antiguo, y sumamos el nuevo
-		this.sumaDeCostes=this.sumaDeCostes-this.coste+nuevoCoste;
+		Enlace.sumaDeCostes=Enlace.sumaDeCostes-this.coste+nuevoCoste;
 		this.coste=nuevoCoste;
 	}
 	public int getSumaDeCostes() {
-		return this.sumaDeCostes;
+		return Enlace.sumaDeCostes;
 	}
 	public int costeEspecial() {
 		return 0;
