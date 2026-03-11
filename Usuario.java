92bcbb353f663be7b88429ac5603c8475f328477
JeanDPG <161874307+JeanDPG@users.noreply.github.com>
2026-03-11 11:55:29 +0100

    Usuario con array de enlaces simplificado

--- Usuario.java
+++ Usuario.java
@@ -6,29 +6,29 @@ public class Usuario {
 
     private String nombre;
     private int capacidadAmp;
-    private Map<Date, Enlace> enlacesSalientes;
+    private List<Enlace> enlacesSalientes;
 
     public Usuario(String nombre) {
         this.nombre = nombre;
         this.capacidadAmp = 2;
-        this.enlacesSalientes = new TreeMap<>();
+        this.enlacesSalientes = new ArrayList<>();
     }
 
     public Usuario(String nombre, int capacidad) {
         this.nombre = nombre;
         this.capacidadAmp = capacidad;
-        this.enlacesSalientes = new TreeMap<>();
+        this.enlacesSalientes = new ArrayList<>();
     }
 
     public boolean addEnlace(Enlace e) {
         if (e.getUsuarioOrigen().equals(this) &&
                 !(e.getUsuarioDestino().equals(this))) {
-            for (Map.Entry<Date, Enlace> entrada : enlacesSalientes.entrySet()) {
-                if (entrada.getValue().getUsuarioDestino().equals(e.getUsuarioDestino())) {
+            for (Enlace enlace: this.enlacesSalientes) {
+                if (enlace.getUsuarioDestino().equals(e.getUsuarioDestino())) {
                     return false;
                 }
             }
-            this.enlacesSalientes.put(new Date(), e);
+            this.enlacesSalientes.add(e);
             return true;
         }
         return false;
@@ -36,12 +36,12 @@ public class Usuario {
 
     public boolean addEnlace(Usuario usuarioDestino, int coste) {
         if (!(usuarioDestino.equals(this))){
-            for (Map.Entry<Date, Enlace> entrada : enlacesSalientes.entrySet()) {
-                if (entrada.getValue().getUsuarioDestino().equals(usuarioDestino)) {
+            for (Enlace enlace : this.enlacesSalientes) {
+                if (enlace.getUsuarioDestino().equals(usuarioDestino)) {
                     return false;
                 }
             }
-            this.enlacesSalientes.put(new Date(),new Enlace(this,usuarioDestino,coste));
+            this.enlacesSalientes.add(new Enlace(this,usuarioDestino,coste));
             return true;
         }
 
@@ -49,10 +49,11 @@ public class Usuario {
     }
     public Enlace getEnlace(Usuario usuarioDestino) {
 
-        for (Enlace e : enlacesSalientes.values()) {
+        for (Enlace e : this.enlacesSalientes) {
             if (e.getUsuarioDestino().equals(usuarioDestino)) {
                 return e;
             }
+            return null;
         }
 
         return null;
@@ -62,7 +63,7 @@ public class Usuario {
             return null;
         }
         int contador = 0;
-        for (Enlace e : enlacesSalientes.values()) {
+        for (Enlace e : this.enlacesSalientes) {
             if (contador == i) {
                 return e;
             }
@@ -82,23 +83,12 @@ public class Usuario {
         return capacidadAmp;
     }
 
-    public Map<Date, Enlace> getEnlacesSalientes() {
+    public List getEnlacesSalientes() {
         return enlacesSalientes;
     }
 
 
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void setCapacidadAmp(int capacidadAmp) {
-        this.capacidadAmp = capacidadAmp;
-    }
-
-    public void setEnlacesSalientes(Map<Date, Enlace> enlacesSalientes) {
-        this.enlacesSalientes = enlacesSalientes;
-    }
 
     @Override
     public String toString() {
@@ -106,11 +96,11 @@ public class Usuario {
         StringBuilder sb = new StringBuilder();
 
         sb.append("@").append(nombre)
-          .append("(").append(capacidadAmp).append(") [");
+                .append("(").append(capacidadAmp).append(") [");
 
         boolean primero = true;
 
-        for (Enlace e : enlacesSalientes.values()) {
+        for (Enlace e : this.enlacesSalientes) {
 
             if (!primero) {
                 sb.append(", ");
