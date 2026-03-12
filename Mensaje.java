1a54c9bac09c9d56b68429ebc1ea15e69caf06d7
JeanDPG <161874307+JeanDPG@users.noreply.github.com>
2026-03-12 11:09:32 +0100

    Mensaje con impresion

--- Mensaje.java
+++ Mensaje.java
@@ -2,69 +2,83 @@ package redSocial;
 
 
 public class Mensaje {
-	private String contenido;
-	private int alcanceDisponible;
-	private Usuario usuarioActual;
-	 public Mensaje(String contenido, int alcanceDisponible, Usuario usuarioActual ) {
-		 this.contenido=contenido;
-		 this.alcanceDisponible=alcanceDisponible;
-		 this.usuarioActual=usuarioActual;
-	 }
-	 public String getContenido() {
-		 return this.contenido;
-	 }
-	 public int getAlcanceDisponible(){
-		 return this.alcanceDisponible;
-	 }
-	 public Usuario getUsuarioActual() {
-		 return this.usuarioActual;
-	 }
-	 public boolean difunde(Enlace e) {
+    private String contenido;
+    private int alcanceDisponible;
+    private Usuario usuarioActual;
+    public Mensaje(String contenido, int alcanceDisponible, Usuario usuarioActual ) {
+        this.contenido=contenido;
+        this.alcanceDisponible=alcanceDisponible;
+        this.usuarioActual=usuarioActual;
+    }
+    public String getContenido() {
+        return this.contenido;
+    }
+    public int getAlcanceDisponible(){
+        return this.alcanceDisponible;
+    }
+    public Usuario getUsuarioActual() {
+        return this.usuarioActual;
+    }
+    public boolean difunde(Enlace e) {
 
-		    if (e != null && puedeDifundirPor(e) && aceptadoPor(e.getUsuarioDestino())) {
+        if (e != null && puedeDifundirPor(e) && aceptadoPor(e.getUsuarioDestino())) {
 
-		        Usuario destino = e.getUsuarioDestino();
+            Usuario destino = e.getUsuarioDestino();
+            Usuario origen = e.getUsuarioOrigen();
+            alcanceDisponible = alcanceDisponible - e.costeReal();
+            if(alcanceDisponible < 1) return false;
+            alcanceDisponible = alcanceDisponible + destino.getCapacidadAmp();
+            origen.getHistorialMensajes().add(this);
+            origen.ajustarExposicion(this);
+            usuarioActual = destino;
+            System.out.println(this);
 
-		        alcanceDisponible = alcanceDisponible - e.costeReal();
-		        alcanceDisponible = alcanceDisponible + destino.getCapacidadAmp();
+            return true;
+        }
 
-		        usuarioActual = destino;
+        return false;
+    }
+    public boolean difunde(Usuario... usuarios) {
 
-		        return true;
-		    }
+        boolean todoCorrecto = true;
 
-		    return false;
-		}
-	 public boolean difunde(Usuario... usuarios) {
+        for (Usuario u : usuarios) {
 
-		    boolean todoCorrecto = true;
+            Enlace e = usuarioActual.getEnlace(u);
 
-		    for (Usuario u : usuarios) {
+            if (e != null) {
 
-		        Enlace e = usuarioActual.getEnlace(u);
+                if (!difunde(e)) {
+                    todoCorrecto = false;
+                }
 
-		        if (e != null) {
+            } else {
+                todoCorrecto = false;
+            }
+        }
 
-		            if (!difunde(e)) {
-		                todoCorrecto = false;
-		            }
+        return todoCorrecto;
+    }
 
-		        } else {
-		            todoCorrecto = false;
-		        }
-		    }
 
-		    return todoCorrecto;
-		}
-	 
-	 public boolean puedeDifundirPor(Enlace e) {
-		    return alcanceDisponible >= e.costeReal();
-		}
-	 public boolean aceptadoPor(Usuario u) {
-		 //Se completara mas adelante con metodos . ahora solo devuelve true
-		 return true;
-	 }
-	 public String toString() {
-		 return "Mensaje ("+contenido+":" + this.alcanceDisponible + ") en @"+ this.usuarioActual.getNombre();
-	 }
+    public int restarCoste(Enlace enlace){
+        this.alcanceDisponible -= enlace.getCoste();
+        return this.alcanceDisponible;
+    }
+
+    public void sumarAmplificacion(Enlace enlace){
+        this.alcanceDisponible += enlace.getUsuarioDestino().getCapacidadAmp();
+    }
+
+    public boolean puedeDifundirPor(Enlace e) {
+        return alcanceDisponible >= e.costeReal();
+    }
+    public boolean aceptadoPor(Usuario u) {
+        //Se completara mas adelante con metodos . ahora solo devuelve true
+        return true;
+    }
+    @Override
+    public String toString() {
+        return "Mensaje ("+contenido+":" + this.alcanceDisponible + ") en @"+ this.usuarioActual.getNombre();
+    }
 }
